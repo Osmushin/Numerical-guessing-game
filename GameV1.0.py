ae807aea771c64b550deb5d783a9f8c678476011
Osmushin <toxa_os@mail.ru>
2023-01-10 14:43:33 +0300

    GameVers1.0

---
+++ GameV1.0.py
@@ -0,0 +1,21 @@
+from random import *
+
+Hidden_num = randint(1, 100)
+print('Добро пожаловать в игру Числовая Угадайка!!')
+
+def is_valid(number):
+    if number.isdigit() and 1 <= int(number) <= 100:
+        return True
+    else:
+        return False
+
+while True:
+    print('Введите число от 1 до 100')
+    number = input()
+
+    if is_valid(number) == True:
+        number = int(number)
+        print('OK')
+        break
+    else:
+        print('А может быть все-таки введем целое число от 1 до 100?')
