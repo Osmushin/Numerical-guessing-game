73751fe0151feb172237d23f7c794493e3141e4d
Osmushin <toxa_os@mail.ru>
2023-01-11 09:34:47 +0300

    Game of Digit V2.0

--- GameV1.0.py
+++ GameV1.0.py
@@ -1,8 +1,6 @@
 from random import *
 
-hidden_num = randint(1, 100)
 print('Добро пожаловать в игру Числовая Угадайка!!')
-
 
 def is_valid(number):
     if number.isdigit() and 1 <= int(number) <= 100:
@@ -10,21 +8,38 @@ def is_valid(number):
     else:
         return False
 
+def game():
+    hidden_num = randint(1, 100)
+    score = 0
+    while True:
+        print('Введите число от 1 до 100')
+        number = input()
 
-while True:
-    print('Введите число от 1 до 100')
-    number = input()
-
-    if is_valid(number) == True:
-        number = int(number)
-        if number < hidden_num:
-            print('Ваше число меньше загаданного, попробуйте еще разок')
-        elif number > hidden_num:
-            print('Ваше число больше загаданного, попробуйте еще разок')
+        if is_valid(number) == True:
+            number = int(number)
+            if number < hidden_num:
+                score += 1
+                print('Ваше число меньше загаданного, попробуйте еще разок')
+            elif number > hidden_num:
+                score += 1
+                print('Ваше число больше загаданного, попробуйте еще разок')
+            else:
+                print('Вы угадали, поздравляем!')
+                print('Количество попыток составило:', score)
+                break
         else:
-            print('Вы угадали, поздравляем!')
-            print('Спасибо что играли в числовую угадайку. Еще увидимся...')
-            break
-    else:
-        print('А может быть все-таки введем целое число от 1 до 100?')
+            print('А может быть все-таки введем целое число от 1 до 100?')
 
+    print('Если хотите сыграть еще раз - введите "да" ')
+    print('Если не хотите продолжать игру введите "стоп"')
+    answer = input()
+    if answer == 'да':
+        return True
+    elif answer == 'стоп':
+        print('Спасибо, что играли в числовую угадайку. Еще увидимся...')
+        return False
+
+game()
+
+while game() == True:
+    game()
